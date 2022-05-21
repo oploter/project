f61224009b3b63df9cf3cd0d3dbca1d29144a995
Andrey <pndreya@gmail.com>
2022-05-21 04:14:52 +0300

    clang-format files and changed font

--- map.cpp
+++ map.cpp
@@ -8,24 +8,23 @@ Map::Map(int rows_, int cols_)
     : rows(rows_), cols(cols_),
       field(rows, std::vector<BlockType>(cols, BlockType::GREEN)) {}
 Map::Map(const std::string &file_name) {
-    std::ifstream file;
-    file.exceptions(std::ios_base::badbit | std::ios_base::failbit);
-    try {
-        file.open(file_name);
-    } catch (...) {
-        std::cout << "Cannot open: " << file_name << "\n";
-        return;
+  std::ifstream file;
+  file.exceptions(std::ios_base::badbit | std::ios_base::failbit);
+  try {
+    file.open(file_name);
+  } catch (...) {
+    std::cout << "Cannot open: " << file_name << "\n";
+    return;
+  }
+  file >> rows >> cols;
+  field.resize(rows, std::vector<BlockType>(cols, BlockType::GREEN));
+  for (int row = 0; row < rows; row++) {
+    for (int col = 0; col < cols; col++) {
+      char type;
+      file >> type;
+      field[row][col] = static_cast<BlockType>(static_cast<int>(type) - 48);
     }
-    file >> rows >> cols;
-    field.resize(rows, std::vector<BlockType>(cols, BlockType::GREEN));
-    for (int row = 0; row < rows; row++) {
-        for (int col = 0; col < cols; col++) {
-            char type;
-            file >> type;
-            field[row][col] =
-                static_cast<BlockType>(static_cast<int>(type) - 48);
-        }
-    }
+  }
 }
 int Map::getRows() const { return rows; }
 int Map::getCols() const { return cols; }
